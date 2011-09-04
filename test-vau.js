de395038a25bf3043a5e7c47beb5dbf57852f877
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2011-09-04 10:42:50 -0400

    Update test-vau for vau2

--- test-vau.js
+++ test-vau.js
@@ -1,15 +1,17 @@
-load("vau.js");
+load("vau2.js");
 
 var t_env = Vau.extend(Vau.baseenv);
 
 function t(exp) {
     try {
 	print(uneval(exp));
-	print(uneval(Vau.eval(exp, t_env)));
+	var result = Vau.eval(exp, t_env);
+	print(uneval(result));
     } catch (e) {
 	print("EXCEPTION: " + uneval(e));
     }
     print();
+    return result;
 }
 
 function te(str) {
@@ -20,15 +22,21 @@ function te(str) {
 	print("EXCEPTION: " + uneval(e));
 	return;
     }
-    t(exp);
+    return t(exp);
+}
+
+function tx(str, expected) {
+    var result = te(str);
+    if (result !== expected) {
+	print("MISMATCH: " + uneval(result) + " !== " + uneval(expected));
+	print();
+    }
 }
 
 var $define = new Vau.Symbol("$define!");
 var $lambda = new Vau.Symbol("$lambda");
 var $begin = new Vau.Symbol("$begin");
-var $$rest = new Vau.Keyword("#rest");
 var $vau = new Vau.Symbol("$vau");
-var $let = new Vau.Symbol("$let");
 var $if = new Vau.Symbol("$if");
 var listStar = new Vau.Symbol("list*");
 var wrap = new Vau.Symbol("wrap");
@@ -45,34 +53,38 @@ try {
     print(uneval(Vau.read(" -123.45e2")));
     print(uneval(Vau.read("(this [is] legal)"))); // not yet it isn't
     // I'd like to reserve [...] for Clojure-style (list ...) syntax.
-    print(uneval(Vau.read("(#rest #ignore)")));
+    print(uneval(Vau.read("(#keyword #ignore)")));
     print(uneval(Vau.read("($define! (hello) \"world\") (rest)")));
 } catch (e) {
     print(uneval(e));
 }
 
 t("hi");
-t(123);
-t([$begin, 123, 234]);
-t([$define, _print, print]);
-t([_print, "hello", "there"]);
-t([$let, [[x, 123]], [_print, "hi"], [_print, "there"], x]);
-t([$if, true, 1, 2]);
-t([$if, false, 1, 2]);
+te("123");
+te("($begin 123 234)");
+t(Vau.arrayToList([$define, _print, print]));
+t(Vau.arrayToList([_print, "hello", "there"]));
+t(Vau.arrayToList([$if, true, 1, 2]));
+t(Vau.arrayToList([$if, false, 1, 2]));
+te("(list* 1 2 3)");
 
-t([$define, $lambda,
-   [$vau, [formals, $$rest, body], dynenv,
-    [wrap,
-     [_eval, [listStar, $vau, formals, Vau.ignore, body], dynenv]]]]);
+te('($define! $lambda ($vau (formals . body) dynenv (wrap (eval (list* $vau formals #ignore body) dynenv))))');
+te('($define! list ($lambda x x))');
+te('($define! cadr ($lambda (v) (car (cdr v))))');
+te('(list 1 2 3)');
+te('($define! map ($lambda (f xs) ($if (null? xs) xs (cons (f (car xs)) (map f (cdr xs))))))');
+te('(map car (list (list 1 2) (list 2 3)))');
+te('($define! $let ($vau (bindings . body) env (eval (list* (list* $lambda (map car bindings) body) (map cadr bindings)) env)))');
+tx('($let ((x 123)) (print "hi") (print "there") x)', 123);
 
 te('(print "hello")');
 te('($lambda (x) x)');
-te('(($lambda (x) x) 123)');
+tx('(($lambda (x) x) 123)', 123);
 te('($define! first ($lambda (f s) f))');
 te('($define! second ($lambda (f s) s))');
-te('(first 123 234)');
-te('(second 123 234)');
-te('"hello \\"world\\" \\\\ foo"');
+tx('(first 123 234)', 123);
+tx('(second 123 234)', 234);
+tx('"hello \\"world\\" \\\\ foo"', "hello \"world\" \\ foo");
 te('#ignore');
-te('#t');
-te('#f');
+tx('#t', true);
+tx('#f', false);
